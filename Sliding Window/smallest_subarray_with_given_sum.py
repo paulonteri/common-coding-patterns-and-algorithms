b2cf6c9a5c34605c06a9314959ac41f02ae19b3d
Paul Onteri <onteripaul@gmail.com>
2020-11-10 18:14:52 +0300

    Smallest subarray sum tests

--- Sliding Window/smallest_subarray_with_given_sum.py
+++ Sliding Window/smallest_subarray_with_given_sum.py
@@ -27,6 +27,18 @@ def smallest_subarray_with_given_sum(s, arr):
     return smallest_len
 
 
+def main():
+    print("Smallest subarray length: " +
+          str(smallest_subarray_with_given_sum(7, [2, 1, 5, 2, 3, 2])))
+    print("Smallest subarray length: " +
+          str(smallest_subarray_with_given_sum(7, [2, 1, 5, 2, 8])))
+    print("Smallest subarray length: " +
+          str(smallest_subarray_with_given_sum(8, [3, 4, 1, 1, 6])))
+
+
+main()
+
+
 """
 SOLUTION:
 First, we will add-up elements from the beginning of the array until their sum becomes greater than or equal to ‘S.’
