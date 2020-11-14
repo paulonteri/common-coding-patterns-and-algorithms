4f001d48e8cbffe5b8159e6d64670f9fb2f61e84
Paul Onteri <onteripaul@gmail.com>
2020-11-14 08:39:32 +0300

    Start of LinkedList Cycle

---
+++ Fast & Slow Pointers/find_cycle_start.py
@@ -0,0 +1,122 @@
+"""
+Start of LinkedList Cycle
+Given the head of a Singly LinkedList that contains a cycle,
+ write a function to find the starting node of the cycle.
+"""
+
+from __future__ import print_function
+
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+    def print_list(self):
+        temp = self
+        while temp is not None:
+            print(temp.value, end='')
+            temp = temp.next
+        print()
+
+
+# Explanation -> https://youtu.be/UmudS7EXz6o
+# O(N) time | O(1) space
+def find_cycle_start(head):
+    slow, fast = head, head
+    while (fast is not None and fast.next is not None):
+        fast = fast.next.next
+        slow = slow.next
+        if slow == fast:
+            return return_start(head, fast)
+    return head
+
+
+def return_start(head, meeting_point):
+    p_one = head
+    p_two = meeting_point
+    while True:
+        if p_one == p_two:
+            # found cycle start
+            return p_one
+        p_one = p_one.next
+        p_two = p_two.next
+
+
+# O(N) time | O(N) space
+def find_cycle_start_2(head):
+    pos = head
+    store = {}
+    while pos != None:
+        if pos in store:
+            return pos
+        store[pos] = True
+        pos = pos.next
+
+    return head
+
+
+# O(N) time | O(1) space
+def find_cycle_start_3(head):
+    cycle_length = 0
+    # find the LinkedList cycle
+    slow, fast = head, head
+    while (fast is not None and fast.next is not None):
+        fast = fast.next.next
+        slow = slow.next
+        if slow == fast:  # found the cycle
+            cycle_length = calculate_cycle_length(slow)
+            break
+    return find_start(head, cycle_length)
+
+
+def calculate_cycle_length(slow):
+    current = slow
+    cycle_length = 0
+    while True:
+        current = current.next
+        cycle_length += 1
+        if current == slow:
+            break
+    return cycle_length
+
+
+def find_start(head, cycle_length):
+    pointer1 = head
+    pointer2 = head
+    # move pointer2 ahead 'cycle_length' nodes
+    while cycle_length > 0:
+        pointer2 = pointer2.next
+        cycle_length -= 1
+    # increment both pointers until they meet at the start of the cycle
+    while pointer1 != pointer2:
+        pointer1 = pointer1.next
+        pointer2 = pointer2.next
+    return pointer1
+
+
+def main():
+    head = Node(1)
+    head.next = Node(2)
+    head.next.next = Node(3)
+    head.next.next.next = Node(4)
+    head.next.next.next.next = Node(5)
+    head.next.next.next.next.next = Node(6)
+
+    head.next.next.next.next.next.next = head.next.next
+    print("LinkedList cycle start: " + str(find_cycle_start_2(head).value))
+    print("LinkedList cycle start: " + str(find_cycle_start_3(head).value))
+    print("LinkedList cycle start: " + str(find_cycle_start(head).value))
+
+    head.next.next.next.next.next.next = head.next.next.next
+    print("LinkedList cycle start: " + str(find_cycle_start_2(head).value))
+    print("LinkedList cycle start: " + str(find_cycle_start_3(head).value))
+    print("LinkedList cycle start: " + str(find_cycle_start(head).value))
+
+    head.next.next.next.next.next.next = head
+    print("LinkedList cycle start: " + str(find_cycle_start_2(head).value))
+    print("LinkedList cycle start: " + str(find_cycle_start_3(head).value))
+    print("LinkedList cycle start: " + str(find_cycle_start(head).value))
+
+
+main()
