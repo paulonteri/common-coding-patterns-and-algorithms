a46b2989b4733de5d79076cd52c67ff29e3ca384
Paul Onteri <onteripaul@gmail.com>
2020-11-24 09:45:45 +0300

    All Paths for a Sum

--- Tree Depth First Search/all_paths_for_sum.py
+++ Tree Depth First Search/all_paths_for_sum.py
@@ -13,34 +13,40 @@ class TreeNode:
 
 
 def find_paths(root, s):
+    if not root:
+        return []
+
     allPaths = []
     # find paths recursively
     _path_finder(root, s, [], allPaths)
     return allPaths
 
 
-def _path_finder(curr_node, required_sum, current_path, allPaths):
-    if curr_node is None:
+def _path_finder(curr, s, curr_path, allPaths):
+    if curr is None:
         return
 
-    current_path.append(curr_node.val)  # add the current node to the path
-    required_sum -= curr_node.val
+    # # gives us eror solvalble by backtracking
+    # curr_path.append(curr.val)
+    # curr_path  += [curr.val]
+    curr_path = curr_path + [curr.val]
+    s = s - curr.val
 
-    # if the current node is a leaf and its value is equal to required_sum, save the current path
-    if required_sum == 0 and not curr_node.left and not curr_node.right:
-        # print(type(current_path))
-        # print(current_path)
-        # allPaths.append(current_path) # add's empy list ???
-        allPaths.append(list(current_path))
+    # if the current node is a leaf and subrating it from s will give you 0, we have found a path, save the current path
+    if s == 0 and not curr.left and not curr.right:
+        # add's empy list ??? when I use curr_path.append(curr.val)
+        allPaths.append(curr_path)
+        # allPaths.append(list(curr_path)) # use this instead when using curr_path.append(curr.val)
 
     else:
         # traverse the left & right sub-tree
-        _path_finder(curr_node.left, required_sum, current_path, allPaths)
-        _path_finder(curr_node.right, required_sum, current_path, allPaths)
+        _path_finder(curr.left, s, curr_path, allPaths)
+        _path_finder(curr.right, s, curr_path, allPaths)
 
+    # # incase we use curr_path.append(curr.val) instead of curr_path = curr_path + [curr.val] above, we will have to backtrack
     # remove the current node from the path to backtrack,
     # we need to remove the current node while we are going up the recursive call stack.
-    del current_path[-1]
+    # del curr_path[-1]
 
 
 def main():
@@ -54,6 +60,9 @@ def main():
     sum = 23
     print("Tree paths with sum " + str(sum) +
           ": " + str(find_paths(root, sum)))
+    sum2 = 18
+    print("Tree paths with sum2 " + str(sum2) +
+          ": " + str(find_paths(root, sum2)))
 
 
 main()
